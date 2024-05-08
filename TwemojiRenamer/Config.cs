ce4b1416b5bf8d19d0fa1cec88a7f678b036056d
matt-edmondson <matthew.edmondson@gmail.com>
2024-05-08 12:46:19 +1000

    Initial commit

---
+++ TwemojiRenamer/Config.cs
@@ -0,0 +1,14 @@
+﻿namespace ktsu.io.TwemojiRenamer;
+
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
+internal class Config
+{
+	public string SourcePath { get; set; } = string.Empty;
+	public string DestinationPath { get; set; } = string.Empty;
+	public Collection<string> Codepoints { get; set; } = new();
+
+	internal static Config Load(string path) =>
+		JsonSerializer.Deserialize<Config>(File.ReadAllText(path)) ?? new();
+}
---
+++ TwemojiRenamer/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ktsu.io.TwemojiRenamer;
+
+using EmojiHelper;
+using ktsu.io.CaseConverter;
+
+internal class Program
+{
+	internal static void Main(string[] args)
+	{
+		if (args.Length != 1)
+		{
+			Console.WriteLine("Provide config file path");
+			return;
+		}
+
+		string configPath = args[0];
+		if (!File.Exists(configPath))
+		{
+			Console.WriteLine("Config file not found");
+			return;
+		}
+
+		var config = Config.Load(configPath);
+		foreach (string codepoint in config.Codepoints)
+		{
+			string oldPath = Path.Combine(config.SourcePath, $"{codepoint}.svg");
+			string name = EmojiHelper.GetEmojiName(codepoint);
+			string filename = name.ToKebabCase();
+			string newPath = Path.Combine(config.DestinationPath, $"{filename}.svg");
+
+			try
+			{
+				File.Copy(oldPath, newPath, overwrite: true);
+				Console.WriteLine($"Copied {codepoint} to {filename}");
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine($"File not found: {oldPath}");
+			}
+			catch (DirectoryNotFoundException)
+			{
+				Console.WriteLine($"Directory not found: {config.DestinationPath}");
+			}
+		}
+	}
+}
